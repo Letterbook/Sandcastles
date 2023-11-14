c4a21531004fd60ea51a139193c4528bc664ecb6
Jenniferplusplus <contact@jenniferplusplus.com>
2023-11-13 21:03:32 -0600

    use localhost for trust script, it works better

--- trust.bash
+++ trust.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 CA_FINGERPRINT=$(docker --log-level ERROR compose --progress quiet run --rm --quiet-pull fingerprint)
-step ca bootstrap --ca-url https://root-ca.castle:9000 --fingerprint $CA_FINGERPRINT --install
+step ca bootstrap --ca-url https://localhost:9000 --fingerprint $CA_FINGERPRINT --install
